71c572cae16426808c15860f9afcf9b639f3e526
financeRyu <fb0982@naver.com>
2026-04-29 22:50:05 +0900

    study: SET, REMOVE 구문 관련 테스트 2026-04-29 류재정

---
+++ test/sec1/remove_test.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+from langchain_neo4j import Neo4jGraph
+
+load_dotenv()
+graph = Neo4jGraph(
+    url=os.getenv("NEO4J_URI"),
+    username=os.getenv("NEO4J_USERNAME"),
+    password=os.getenv("NEO4J_PASSWORD"),
+)
+
+
+def main():
+    query = """\
+    MATCH (p:Person {name: "홍길동"})
+    REMOVE p.email        
+    RETURN p    
+    """
+    result = graph.query(query)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
